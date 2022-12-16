172c133fee1199dff3e7d225c070e74d03056226
KoyoiDev <koyoi.dev@gmail.com>
2022-12-17 02:09:26 +0700

    restore migrations

---
+++ database/migrations/2022_12_16_190655_create_cart_stock_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_stock', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cart_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('stock_id')->constrained()->cascadeOnDelete();
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cart_stock', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('cart_id');
+            $table->dropConstrainedForeignId('stock_id');
+        });
+        Schema::dropIfExists('cart_stock');
+    }
+}
